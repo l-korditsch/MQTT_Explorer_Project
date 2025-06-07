b4d428d2a025fb5d36d3a7e9c8586b0708ef5e36
Smurx <lucaskorditsch@gmail.com>
2025-06-07 13:23:57 +0200

    Removed TODO comments for auto scaling and interface improvements

--- Main.py
+++ Main.py
@@ -1,9 +1,6 @@
 import tkinter as tk
 from frontend import MQTTFrontend
 
-# TODO Add auto scaling of HUD to window size
-# TODO Prettefy the whole interface
-
 if __name__ == "__main__":
     root = tk.Tk()
     app = MQTTFrontend(root)
